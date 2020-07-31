8ea0ebf7b8600ec9f1e0e4770035c236da9a7bdd
Rafael <rfl777@mail.ru>
2020-07-31 10:35:04 +0300

    Merge branch 'master' into jdbcconnection
    # Conflicts:
    #	src/main/java/org/javalearncourse/bookshop/beans/Product.java
    #	src/main/java/org/javalearncourse/bookshop/services/ProductServiceImpl.java

--- src/main/java/org/javalearncourse/bookshop/beans/PrintEdition.java
+++ src/main/java/org/javalearncourse/bookshop/beans/PrintEdition.java
@@ -1,12 +1,51 @@
 package org.javalearncourse.bookshop.beans;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class PrintEdition {
-    private  int id;
+    @Id
+    @GeneratedValue (strategy=GenerationType.AUTO)
+    private  Integer id;
 
     private String title;
     private String author;
     private double price;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
     public PrintEdition(int id, String title, String author, double price) {
         this.id = id;
---
+++ src/main/java/org/javalearncourse/bookshop/repos/PrintEditionRepo.java
@@ -0,0 +1,8 @@
+package org.javalearncourse.bookshop.repos;
+
+import org.javalearncourse.bookshop.beans.PrintEdition;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PrintEditionRepo extends CrudRepository<PrintEdition, Integer> {
+
+}
